7cdfccd64a42b49ac3e1537c6f5ee5a716889de5
enzoenrico <e.enrico2005@gmail.com>
2024-09-11 10:55:14 +0000

    updated PyPDF2 version

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import requests
 
 def make_post_request_with_pdf(filepath):
-    url = "http://localhost:8000/ponga"
+    # url = "http://localhost:8000/ponga"
+    url = "https://render-pasq-api.onrender.com/process"
     # files = {'post_file': open(filepath, 'rb')}
     with open(filepath, "rb") as f:
         r = requests.post(url, files={"post_file": f})
