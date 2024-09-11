6595dc84070c8c3bf87a797abacd6c9ebd3d7066
enzoenrico <e.enrico2005@gmail.com>
2024-09-11 11:46:12 +0000

    temp change on /process route, for testing

--- main.py
+++ main.py
@@ -23,41 +23,40 @@ async def wake_up():
     return {"message": "Waking up"}, 200
 
 
-@app.post('/process')
-async def process(post_file: UploadFile = File(...)):
-    c = camelot.read_pdf("./Relatório_de_fabricação_por_módulo_Projeto_Balcao.pdf", pages='1-end', flavor="stream")
+# @app.get("/process")
+# async def process(post_file: UploadFile = File(...)):
+#     c = camelot.read_pdf("./Relatório_de_fabricação_por_módulo_Projeto_Balcao.pdf", pages='1-end', flavor="stream")
 
-    headers: List[str] = []
-    objects: List[Part] = []
-    present_parent = ""
+#     headers: List[str] = []
+#     objects: List[Part] = []
+#     present_parent = ""
 
-    for table in c:
-        # Convert the table to a list of dictionaries
-        parsed_data: List[Dict[str, str]] = table.df.to_dict(orient='records')
+#     for table in c:
+#         # Convert the table to a list of dictionaries
+#         parsed_data: List[Dict[str, str]] = table.df.to_dict(orient='records')
 
-        for obj in parsed_data:
-            # If first item is not empty and others are empty, check if header item
-            if obj["0"] != "" and all(obj[str(index)] == "" for index in range(1, 5)):
-                headers.append(obj["0"])
-                present_parent = obj["0"]
-            if present_parent != "" and all(obj[str(index)] != "" for index in range(len(obj))):
-                # If the ref has a '.', it's valid
-                if "." in obj["1"].__str__():
-                    parsed_part = Part(
-                        parent=present_parent,
-                        code=obj["0"],
-                        ref=obj["1"],
-                        quantity=obj["4"],
-                        size=obj["3"]
-                    )
-                    objects.append(parsed_part)
-                    pprint(parsed_part.to_json())
+#         for obj in parsed_data:
+#             # If first item is not empty and others are empty, check if header item
+#             if obj["0"] != "" and all(obj[str(index)] == "" for index in range(1, 5)):
+#                 headers.append(obj["0"])
+#                 present_parent = obj["0"]
+#             if present_parent != "" and all(obj[str(index)] != "" for index in range(len(obj))):
+#                 # If the ref has a '.', it's valid
+#                 if "." in obj["1"].__str__():
+#                     parsed_part = Part(
+#                         parent=present_parent,
+#                         code=obj["0"],
+#                         ref=obj["1"],
+#                         quantity=obj["4"],
+#                         size=obj["3"]
+#                     )
+#                     objects.append(parsed_part)
+#                     pprint(parsed_part.to_json())
 
-        with open('output.json', 'w') as outfile:
-            json.dump(PartsList(name=present_parent, parts=objects).to_json(), outfile)
+#         with open('output.json', 'w') as outfile:
+#             json.dump(PartsList(name=present_parent, parts=objects).to_json(), outfile)
 
-        return PartsList(name="test", parts=objects).to_json()
-#     asyncio.run(process())
+#         return PartsList(name="test", parts=objects).to_json()
 
 @app.post("/ponga")
 async def ponga(post_file:UploadFile = File(...)):
@@ -68,6 +67,17 @@ async def ponga(post_file:UploadFile = File(...)):
         print(post_file.filename)
         # tables = camelot.read_pdf("./Relatório_de_fabricação_por_módulo_Projeto_Balcao.pdf", pages='1-end', flavor="stream")
         tables = camelot.read_pdf(temp_file_path, pages='1-end', flavor="stream")
+        contents = []
+        for table in tables:
+            contents.append(table.df.to_dict(orient='records'))
+        # print(contents)
+        return contents
+
+
+@app.get("/process")
+async def process():
+        # tables = camelot.read_pdf("./Relatório_de_fabricação_por_módulo_Projeto_Balcao.pdf", pages='1-end', flavor="stream")
+        tables = camelot.read_pdf("./Relatório_de_fabricação_por_módulo_Projeto_Balcao.pdf", pages='1-end', flavor="stream")
         contents = []
         for table in tables:
             contents.append(table.df.to_dict(orient='records'))
